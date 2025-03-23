5db290793c6afbd375bbc9b59cb9ed0388105ab4
Sathyanesar <sathyanesar3103@gmail.com>
2025-03-23 20:18:52 +0530

    Database.java

--- src/trainreservation/Database.java
+++ src/trainreservation/Database.java
@@ -3,8 +3,8 @@ import java.sql.*;
 
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/train_reservation";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "sathya";
+    private static final String USERNAME = "YOUR USERNAME";
+    private static final String PASSWORD = "YOUR PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
